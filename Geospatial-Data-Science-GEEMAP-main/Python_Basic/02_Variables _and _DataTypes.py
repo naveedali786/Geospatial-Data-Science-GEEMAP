a15e3971212a09b87e17573602d5709c12479b64
Muhammad Naveed <106032776+naveedali786@users.noreply.github.com>
2024-03-15 22:57:40 +0500

    Add files via upload

---
+++ Geospatial-Data-Science-GEEMAP-main/Python_Basic/02_Variables _and _DataTypes.py
@@ -0,0 +1,61 @@
+# There are Many Type of variabel , we cover basic 
+# 1. int
+# 2. Flot
+# 3. String 
+
+# Let declear a variable of int
+num = 4
+print(num)
+
+# Now let's declare a float number-
+num_Float=120.5
+print(num_Float)
+
+# Now let's declare a String 
+
+Name='Muhammad Naveed'
+print(Name)
+
+# Print statementmain ek string use aur variable 
+print('My Name is :',Name)
+
+# Rule of Varaible 
+# Rules To Assign a variables
+# 1: The varaible should be contain letters, numbers or underscores
+# 2: Do not start  with Number
+# 3: Spaces are not allowed
+# 4: Do not use keyword used in function (break,media,test,print,etc)
+# 5: variable name is short and descriptive
+# 6:  Case senstivity( lower case ,Uppercase ,I suggestion you you wrting should write in lower case)
+
+
+#Multi variable in oneline
+x, y, z = 45,67,45
+print(x)
+
+
+#example of varaible
+fruit_basket='mangeos, Orange,banana'
+print(fruit_basket)
+print(type(fruit_basket))
+
+# Add More 5 exsmple 
+
+# Other Tips for Naming of variable
+# 1. only those jo python ki apni zuban main nahi hyn
+# 2. Do not use Reserved words or keywords
+# 3. Do not use captilize  varaible name
+# 4. Alwasy use short word
+# 5. donot ever write variable names with space instead use underscore
+# 6. Special character donot use like & % @
+# 7. Do not use number in varaible at beginning
+# 8. Meaningful
+# 9.  Global trends 
+# 10. Do not repeat the same variable 
+# 11. Do not use operator  use  like (+ - / * ** ^)
+
+# adavage of varaible
+# 1: it is reduces the human effort for codinng
+#2:  easy to write for long datasets
+# 3: Easy to recall
+
