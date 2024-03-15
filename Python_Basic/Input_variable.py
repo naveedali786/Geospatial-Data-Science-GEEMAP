a5434310d6ea83644b449a6ff21953cbcb3f0545
Muhammad Naveed <106032776+naveedali786@users.noreply.github.com>
2024-03-15 23:04:28 +0500

    Add files via upload

---
+++ Python_Basic/Input_variable.py
@@ -0,0 +1,23 @@
+# # input varaible which is take to help input from user
+
+# # to use input functions
+# fruit_basket=input("What is you favourite fruite .?")
+# print(fruit_basket)
+
+# # input function of 32nd stage
+# name= input("What is your name")
+# Greetings="Hello"
+# print(Greetings,name)
+
+
+# # an other way of input function 
+# name= input("What is your name")
+
+# print("Hello",name)
+
+# # An other way of use input function  
+# name=input("What is you name ?")
+# age=inpt("How old are you ?")
+# greeting="Hello"
+# print(greeting,name, " :You are still young")
+
